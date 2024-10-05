b5ac51966983a24e00e72cbaf6991c4dae467815
Flávia Correia <fllaviacorreia@outlook.com>
2024-10-05 10:48:35 -0300

    create login validation

---
+++ src/validates/login.ts
@@ -0,0 +1,12 @@
+import { boolean, object, string } from 'yup';
+
+export const LoginSchema = object({
+  email: string()
+    .email("Formato de e-mail inválido.")
+    .trim()
+    .required("E-mail é obrigatório."),
+  password: string()
+    .min(8, "Necessário possuir mínimo de 8 caracteres.")
+    .trim()
+    .required("Senha é obrigatória."),
+});
