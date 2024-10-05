66481ac0d0ea2a915f4db57b465b659ccdf0e4f1
Flávia Correia <fllaviacorreia@outlook.com>
2024-10-05 10:46:08 -0300

    create theme.ts

---
+++ theme.ts
@@ -0,0 +1,13 @@
+const theme = {
+    colors: {
+      secondary: '#8DB580',  // Secondary color
+      warning: '#D1495B',    // Warning color
+      text: '#0E101A',       // Text color
+      primary: '#9893bc',    // Primary color
+      background: '#FBF2EE', // Background color
+      gray: '#BFBFBF',       // Gray color
+    },
+  };
+  
+  export default theme;
+  
